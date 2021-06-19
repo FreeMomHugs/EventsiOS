44ebb756b08f3abd2094c93347f37618e0ecf56d
Preston Kemp <preston.kemp.1@us.af.mil>
2021-06-19 11:48:56 -0500

    Initial Commit

---
+++ Events/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Events
+//
+//  Created by Preston Kemp on 6/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Events/EventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventsApp.swift
+//  Events
+//
+//  Created by Preston Kemp on 6/19/21.
+//
+
+import SwiftUI
+
+@main
+struct EventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
